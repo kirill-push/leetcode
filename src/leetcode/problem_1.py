0d3f11fe68abbbbd036903239e2c11b6ad5e2653
Kirill Pushkarev <kirillpush@yandex.ru>
2023-10-25 19:18:02 +0300

    style fixes

--- src/leetcode/problem_1.py
+++ src/leetcode/problem_1.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         table = dict()
-        for i in range(len(nums)):
-            if nums[i] in table:
-                return [table[nums[i]], i]
-            table[target - nums[i]] = i
+        for i, n in enumerate(nums):
+            if n in table:
+                return [table[n], i]
+            table[target - n] = i
